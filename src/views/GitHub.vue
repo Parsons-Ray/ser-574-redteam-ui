00749767435d8421380a725b9619d78bee8564a3
Parsons-Ray <32499337+Parsons-Ray@users.noreply.github.com>
2019-02-20 19:04:28 -0700

    Update GitHub.vue

--- src/views/GitHub.vue
+++ src/views/GitHub.vue
@@ -11,9 +11,9 @@
     import {USER_STORIES} from "@/constants";
     import {PROCESSES} from "@/constants";
     import {GRADIENT} from "@/constants";
-    import CircleChart from "@/components/CircleChart";
-    import SparkLine from "@/components/SparkLine";
-    import BarChart from "@/components/BarChart";
+    import xline from "@/components/xline";
+    import xhbar from "@/components/xhbar";
+    
     export default {
         name: "Github",
         components: {Tab},
@@ -21,7 +21,7 @@
             return {
                 title: "Github Statistic",
                 tabs: {
-                    'Commits': {data: USER_STORIES, component: xcircle, headers: HEADERS, title: 'Commits in Tasks'},
+                    'Commits': {data: USER_STORIES, component: DataTable, headers: HEADERS, title: 'Commits during the development'},
                     'Contribution': {data: PROCESSES, component: xhbar, title: 'Contributions', headers: null},
                     'Process': {data: PROCESSES, component: xline, title: 'User Stories', headers: null},
                 }

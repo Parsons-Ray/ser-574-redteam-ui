cf5b901ef0fe561755f1f8ce42077341c0ab4a59
Parsons-Ray <32499337+Parsons-Ray@users.noreply.github.com>
2019-03-18 01:37:05 -0700

    #47 Task 36 Pie Chart for code complexity

---
+++ src/components/CodeComplexity.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <vue-easy-pie-chart :percent="70"></vue-easy-pie-chart>
+        <vue-easy-pie-chart :percent="90"></vue-easy-pie-chart>
+        <vue-easy-pie-chart :percent="60"></vue-easy-pie-chart>
+        <vue-easy-pie-chart :percent="75"></vue-easy-pie-chart>
+        <vue-easy-pie-chart :percent="68"></vue-easy-pie-chart>
+    </div>
+</template>
+
+<script>
+    import VueEasyPieChart from 'vue-easy-pie-chart'
+    import 'vue-easy-pie-chart/dist/vue-easy-pie-chart.css'
+
+    export default {
+        components:{ VueEasyPieChart }
+    }
+</script>

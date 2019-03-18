6c95e83112f5551cf708ea19370f5afd09c66b0e
Parsons-Ray <32499337+Parsons-Ray@users.noreply.github.com>
2019-03-18 01:36:01 -0700

    #47 Task 36 Pie Chart for code complexity
    update Github.Vue

---
+++ src/components/GitHub.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+            <Tab v-bind:tabTitles=tabs v-bind:toolbarTitle="title"></Tab>
+    </div>
+</template>
+
+<script>
+    import Tab from "@/components/Tab";
+    import DataTable from "@/components/DataTable";
+    //import {HEADERS} from "@/constants";
+    //import {USER_STORIES} from "@/constants";
+    //import {PROCESSES} from "@/constants";
+    //import {GRADIENT} from "@/constants";
+    //import SparkLine from "@/components/SparkLine";
+    //import BarChart from "@/components/BarChart";
+    import GithubContribution from "../tabs/GithubContribution"
+    import {HEADERS1} from "../constants";
+    import {MEMBERS} from "../constants";
+    import {COMPLEXITY} from "../constants";
+    import {PIEDATA} from "../constants";
+    import CodeComplexity from "../components/CodeComplexity";
+
+    export default {
+        name: "Github",
+        components: {Tab},
+        data() {
+            return {
+                title: "Github Statistic",
+                tabs: {
+                    'Commits': {data: MEMBERS, component: DataTable, headers: HEADERS1, title: 'Commits in Sprints'},
+                    'Code Complexity': {data: COMPLEXITY, component: CodeComplexity},
+                    //'Pull Requests': {data: , component: SparkLine, title: 'Burn Down Chart', headers: null}
+                    //'Comments': {data: PROCESSES, component: BarChart, title: 'User Stories', headers: null}
+                    'Contribution': {data: PIEDATA, component: GithubContribution, }
+                }
+            }
+        }
+    }
+
+</script>
